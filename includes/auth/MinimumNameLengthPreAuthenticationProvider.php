9d6c5bbe23ce8e1ef4d80c9248054658a64712b1
zoranzoki21 <zorandori4444@gmail.com>
2020-09-05 15:56:16 +0200

    Add phpcs and make it pass
    Change-Id: Iad6bb5768318ec7a4ba6d269540e6031ed86f525

--- includes/auth/MinimumNameLengthPreAuthenticationProvider.php
+++ includes/auth/MinimumNameLengthPreAuthenticationProvider.php
@@ -3,16 +3,20 @@
  * Implements the PreAuthenticationProvider interface for the MinimumNameLength extension.
  */
 
-/**
- * Class MinimumNameLengthPreAuthenticationProvider
- */
-class MinimumNameLengthPreAuthenticationProvider extends
-	\MediaWiki\Auth\AbstractPreAuthenticationProvider {
+class MinimumNameLengthPreAuthenticationProvider
+	extends \MediaWiki\Auth\AbstractPreAuthenticationProvider {
+
+	/**
+	 * @param User $user
+	 * @param bool|string $autocreate
+	 * @param array $options
+	 * @return StatusValue
+	 */
 	public function testUserForCreation( $user, $autocreate, array $options = [] ) {
 		global $wgMinimumUsernameLength;
 
 		$value = StatusValue::newGood();
-		if( $autocreate === false && strlen( $user->getName() ) < $wgMinimumUsernameLength ) {
+		if ( $autocreate === false && strlen( $user->getName() ) < $wgMinimumUsernameLength ) {
 			$value->fatal( wfMessage( 'minimumnamelength-error', Message::numParam(
 				$wgMinimumUsernameLength ) ) );
 		}
